7cfdff9b4d74af3c60a44d269a3e9e54df300a24
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2016-06-28 15:12:19 +0200

    Editors are now in dedicated project

--- plugins/org.palladiosimulator.editors.util/src/org/palladiosimulator/editors/util/api/services/ResourceEnvironmentServices.java
+++
@@ -1,9 +0,0 @@
-package org.palladiosimulator.editors.util.api.services;
-
-public class ResourceEnvironmentServices extends PCMServices {
-
-    public ResourceEnvironmentServices() {
-        super();
-    }
-
-}
