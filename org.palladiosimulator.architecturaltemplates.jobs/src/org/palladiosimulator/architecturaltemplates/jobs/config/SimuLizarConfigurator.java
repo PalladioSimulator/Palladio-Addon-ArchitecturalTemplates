c8449b9e941bd2278a387d90ff0bf22d245fbc55
mrombach <matthias.rombach@itk-engineering.de>
2017-04-24 08:32:01 +0200

    configurator scans repository, system, resourcenev and allocation

--- org.palladiosimulator.architecturaltemplates.jobs/src/org/palladiosimulator/architecturaltemplates/jobs/config/SimuLizarConfigurator.java
+++ org.palladiosimulator.architecturaltemplates.jobs/src/org/palladiosimulator/architecturaltemplates/jobs/config/SimuLizarConfigurator.java
@@ -4,18 +4,27 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.osgi.framework.Bundle;
 import org.palladiosimulator.analyzer.workflow.blackboard.PCMResourceSetPartition;
 import org.palladiosimulator.architecturaltemplates.AT;
 import org.palladiosimulator.architecturaltemplates.api.ArchitecturalTemplateAPI;
 import org.palladiosimulator.architecturaltemplates.jobs.constants.ATPartitionConstants;
+import org.palladiosimulator.pcm.repository.Repository;
 import org.palladiosimulator.simulizar.launcher.IConfigurator;
 import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 import org.palladiosimulator.simulizar.utils.FileUtil;
@@ -24,6 +33,11 @@ import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 public class SimuLizarConfigurator implements IConfigurator {
 
+	public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Map<Object,Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+	
     public SimuLizarConfigurator() {
     }
 
@@ -35,7 +49,7 @@ public class SimuLizarConfigurator implements IConfigurator {
     }
 
 	private void copyReconfigurations(final MDSDBlackboard blackboard) {
-		for (final AT architecturalTemplate : getATsFromSystem(blackboard)) {
+		for (final AT architecturalTemplate : getAllATs(blackboard)) {
             if (architecturalTemplate.getReconfigurationRuleFolder() != null) {
                 final String x = architecturalTemplate.getReconfigurationRuleFolder().getFolderURI();
                 final URI[] uris = FileUtil.getQvtoFiles(x);
@@ -92,4 +106,32 @@ public class SimuLizarConfigurator implements IConfigurator {
 
         return ArchitecturalTemplateAPI.getATsFromSystem(system);
     }
+    
+    private Collection<AT> getAllATs(final MDSDBlackboard blackboard) {
+        final PCMResourceSetPartition pcmRepositoryPartition = (PCMResourceSetPartition) blackboard
+                .getPartition(ATPartitionConstants.Partition.PCM.getPartitionId());
+
+        final Collection<Resource> resources = new ArrayList<Resource>();
+        try {
+        	resources.add(pcmRepositoryPartition.getSystem().eResource());
+        	resources.add(pcmRepositoryPartition.getAllocation().eResource());
+        	resources.add(pcmRepositoryPartition.getResourceEnvironment().eResource());
+        	for(final Repository repository : pcmRepositoryPartition.getRepositories()) {
+        		resources.add(repository.eResource());
+        	}
+        } catch (final IndexOutOfBoundsException e) {
+        }
+
+        ArchitecturalTemplateAPI.getATsFromSystem(pcmRepositoryPartition.getSystem());
+        final Collection<AT> allATs = new LinkedList<AT>();
+        for(Resource resource : resources) {
+        	allATs.addAll(ArchitecturalTemplateAPI.getAppliedArchitecturalTemplates(resource));
+        }
+        
+        return removeDuplicates(allATs);
+    }
+    
+    private Collection<AT> removeDuplicates(Collection<AT> allATs) {
+    	return allATs.stream().filter(distinctByKey(at -> at.getId())).collect(Collectors.toList());
+	}
 }
